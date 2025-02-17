fedf50007f3888330fa5b553f87b20072956caec
rishabsanpui <113790209+rishabsanpui@users.noreply.github.com>
2025-02-17 20:42:18 +0530

    Update Blockchain.java

--- blockchain/com/ransomware/blockchain/Blockchain.java
+++ blockchain/com/ransomware/blockchain/Blockchain.java
@@ -2,17 +2,24 @@ package com.ransomware.blockchain;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class Blockchain {
     private List<Block> chain;
+    private int difficulty; // Mining difficulty
 
-    public Blockchain() {
+    public Blockchain(int difficulty) {
+        this.difficulty = difficulty;
         chain = new ArrayList<>();
-        chain.add(new Block("Genesis Block", "0"));
+        // Genesis block (first block)
+        Block genesisBlock = new Block("Genesis Block", "0");
+        genesisBlock.mineBlock(difficulty); // Mine first block
+        chain.add(genesisBlock);
     }
 
     public void addBlock(String data) {
-        Block previousBlock = chain.get(chain.size()-1);
+        Block previousBlock = chain.get(chain.size() - 1);
         Block newBlock = new Block(data, previousBlock.getHash());
+        newBlock.mineBlock(difficulty); // Mining required before adding
         chain.add(newBlock);
     }
 
@@ -22,13 +29,25 @@ public class Blockchain {
             Block previousBlock = chain.get(i - 1);
 
             if (!currentBlock.getHash().equals(currentBlock.calculateHash())) {
-                return false; // Data tampered
+                System.out.println("Block " + i + " has been tampered!");
+                return false;
             }
 
             if (!currentBlock.getPreviousHash().equals(previousBlock.getHash())) {
-                return false; // Chain broken
+                System.out.println("Block " + i + " previous hash mismatch!");
+                return false;
             }
         }
         return true;
     }
+
+    // public void printBlockchain() {
+    //     for (Block block : chain) {
+    //         System.out.println("Block:");
+    //         System.out.println("Data: " + block.getData());
+    //         System.out.println("Hash: " + block.getHash());
+    //         System.out.println("Previous Hash: " + block.getPreviousHash());
+    //         System.out.println("----------------------");
+    //     }
+    // }
 }
